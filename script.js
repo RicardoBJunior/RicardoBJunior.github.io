62961b2cdfd79e8b7fbeb7575f0e812f8ec70629
RicardoBJunior <bertolin42@gmail.com>
2024-08-09 08:22:38 -0300

    Convite2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ noButton.addEventListener('mouseover', function() {
     const container = document.querySelector('.container');
     const containerRect = container.getBoundingClientRect();
     const noButtonRect = noButton.getBoundingClientRect();
+
     const offsetX = Math.random() * (containerRect.width - noButtonRect.width);
     const offsetY = Math.random() * (containerRect.height - noButtonRect.height);
 
